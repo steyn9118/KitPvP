903b0465a44811d66ac9cbf6a928a959ce0ae05b
Steyn91 <steyn9118@gmail.com>
2025-06-17 22:22:26 +0700

    fix PlayerModelController.java

--- src/main/java/steyn91/kitPvP/models/PlayerModelController.java
+++ src/main/java/steyn91/kitPvP/models/PlayerModelController.java
@@ -5,19 +5,19 @@ import java.util.UUID;
 
 public class PlayerModelController {
 
-    HashMap<UUID, PlayerModel> playerModels = new HashMap<>();
+    private static final HashMap<UUID, PlayerModel> playerModels = new HashMap<>();
 
-    public PlayerModel getModelByUUID(UUID uuid) throws Exception{
+    public static PlayerModel getModelByUUID(UUID uuid) throws Exception{
         PlayerModel model = playerModels.get(uuid);
         if (model == null) throw new Exception("Игрок с UUID " + uuid.toString() + " не найден!");
         return model;
     }
 
-    public void putModel(UUID uuid, PlayerModel model){
+    public static void putModel(UUID uuid, PlayerModel model){
         playerModels.put(uuid, model);
     }
 
-    public void removeModel(UUID uuid){
+    public static void removeModel(UUID uuid){
         playerModels.remove(uuid);
     }
 }
