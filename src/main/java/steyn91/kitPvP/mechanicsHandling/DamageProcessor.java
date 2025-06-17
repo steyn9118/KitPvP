8ffbe8bfba0600a3a7c1f64db3499b6619380ca7
Steyn91 <steyn9118@gmail.com>
2025-06-17 22:20:53 +0700

    Написал PlayerModelController.java

--- src/main/java/steyn91/kitPvP/mechanicsHandling/DamageProcessor.java
+++ src/main/java/steyn91/kitPvP/mechanicsHandling/DamageProcessor.java
@@ -1,9 +1,16 @@
 package steyn91.kitPvP.mechanicsHandling;
 
 import org.bukkit.entity.Entity;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
 
-public class DamageProcessor {
+public class DamageProcessor implements Listener {
 
+    @EventHandler
+    public void onDamage(EntityDamageEvent event){
+        event.setCancelled(true);
+    }
 
     // TODO обработка нанесения/получения урона
     public void dealDamage(Entity source, Entity target, int damageAmount){
--- src/main/java/steyn91/kitPvP/models/PlayerModelController.java
+++ src/main/java/steyn91/kitPvP/models/PlayerModelController.java
@@ -1,7 +1,23 @@
 package steyn91.kitPvP.models;
 
+import java.util.HashMap;
+import java.util.UUID;
+
 public class PlayerModelController {
 
+    HashMap<UUID, PlayerModel> playerModels = new HashMap<>();
 
+    public PlayerModel getModelByUUID(UUID uuid) throws Exception{
+        PlayerModel model = playerModels.get(uuid);
+        if (model == null) throw new Exception("Игрок с UUID " + uuid.toString() + " не найден!");
+        return model;
+    }
 
+    public void putModel(UUID uuid, PlayerModel model){
+        playerModels.put(uuid, model);
+    }
+
+    public void removeModel(UUID uuid){
+        playerModels.remove(uuid);
+    }
 }
